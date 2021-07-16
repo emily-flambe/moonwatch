f39ab1315ba7c2b6552e0208a86fb7a87e7ce413
Emily Cogsdill <emilycogsdill@gmail.com>
2021-07-16 16:35:17 -0500

    only post Price if it has changed

--- python_app.py
+++ python_app.py
@@ -19,7 +19,31 @@ executors = {
     "default":ThreadPoolExecutor(max_workers=10)
 }
 
+KEYFILE = 'service_account_creds.json'
+service_account_creds = os.getenv("SERVICE_ACCOUNT_CREDS")
+worksheet_key = os.getenv("MOONWATCH_WORKSHEET_KEY")
+sheet_index = 0
+
 # Functions setup
+
+def authenticateGoogleSheets():
+    '''
+    Returns gc object
+    '''
+
+     # Authenticate Google service account to access Google Sheets
+    try:
+        with open(KEYFILE, "w") as secret_file:
+            secret_file.write(service_account_creds)
+        gc = gspread.service_account(filename=KEYFILE)
+        print("Authentication succeeded, yaaay")
+        os.remove(KEYFILE)
+
+    except:
+        print("Google Sheets authentication failed :( :( :(")
+
+    return gc
+
 def getStomnkPriceDataframe(ticker):
     
     # Get current price of stomnk
@@ -49,22 +73,9 @@ def googleSheetAppendDataframe(new_data_df):
     '''
     Returns Google Sheets `worksheet` object
     '''
-
-    KEYFILE = 'service_account_creds.json'
-    service_account_creds = os.getenv("SERVICE_ACCOUNT_CREDS")
-    worksheet_key = os.getenv("MOONWATCH_WORKSHEET_KEY")
-    sheet_index = 0
     
     # Authenticate
-    try:
-        with open(KEYFILE, "w") as secret_file:
-            secret_file.write(service_account_creds)
-        gc = gspread.service_account(filename=KEYFILE)
-        print("Authentication succeeded, yaaay")
-        #os.remove(KEYFILE)
-
-    except:
-        print("Google Sheets authentication failed :( :( :(")
+    gc = authenticateGoogleSheets()
         
     # Open the worksheet
     sh = gc.open_by_key(worksheet_key)
@@ -94,6 +105,29 @@ def post_message_to_slack(text, blocks = None):
         'blocks': json.dumps(blocks) if blocks else None
     }).json() 
 
+def checkIfPriceUpdated(ticker):
+    '''
+    Returns boolean: TRUE if the price has changed, FALSE if it is the same as last time it was scraped
+    '''
+    
+    # Authenticate
+    gc = authenticateGoogleSheets()
+        
+    # Load the worksheet as a dataframe
+    sh = gc.open_by_key(worksheet_key)
+    worksheet = sh.get_worksheet(sheet_index)
+    sheet_as_df = gd.get_as_dataframe(worksheet).sort_values(by=['Timestamp'])
+    
+    # Filter to rows for specific ticker
+    sheet_as_df = sheet_as_df[sheet_as_df['Ticker']==ticker]
+    
+    # Compare price values in latest row vs. prior row
+    previous_price = sheet_as_df.iloc[len(sheet_as_df)-2:len(sheet_as_df)-1].reset_index()['Price'][0]
+    new_price = sheet_as_df.iloc[len(sheet_as_df)-1:len(sheet_as_df)].reset_index()['Price'][0]
+    
+    # Return boolean comparing previous to latest price (TRU)
+    return previous_price != new_price    
+
 def getStomnkUpdate(ticker):
     
     # Get fresh data for the ticker
@@ -102,19 +136,26 @@ def getStomnkUpdate(ticker):
     # Append data to the Google Sheet
     googleSheetAppendDataframe(new_data_df)
     
-    # Craft a very helpful message
-    message = createSlackMessage(new_data_df)
+    # If the price has changed since last scraped, post a message to Slack
+
+    if checkIfPriceUpdated(ticker):
+        # Craft a very helpful message
+        message = createSlackMessage(new_data_df)
+        print("Price is updated! Blessings abound")
     
-    # Post it to Slack
+    else:
+        message = "HODL :gem: :raised_hands:"
+        print("Price has not changed - HODL")
+
+    # Post the message to Slack
     post_message_to_slack(message, blocks = None)
-    
-    print("All done")
+    print("All done!")
 
 
 def main():
 
     scheduler = BackgroundScheduler(executors=executors)
-    scheduler.add_job(getStomnkUpdate, 'interval', seconds=600, args=["GME"])
+    scheduler.add_job(getStomnkUpdate, 'interval', seconds=10, args=["GME"])
     scheduler.start()
 
     try:
