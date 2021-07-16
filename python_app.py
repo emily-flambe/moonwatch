b46f010b424188c54c7e55c202f8b2dedc311f5f
Emily Cogsdill <emilycogsdill@gmail.com>
2021-07-16 15:36:24 -0500

    Update python_app.py

--- python_app.py
+++ python_app.py
@@ -4,6 +4,11 @@ import time
 import bs4
 import requests
 import logging
+import pandas as pd
+import gspread
+import gspread_dataframe as gd
+from gspread_dataframe import set_with_dataframe
+from datetime import date,datetime
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -15,6 +20,63 @@ executors = {
 }
 
 # Functions setup
+def getStomnkPriceDataframe(ticker):
+    
+    # Get current price of stomnk
+    url = f'https://finance.yahoo.com/quote/{ticker}?p={ticker}&.tsrc=fin-srch'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    price = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+    
+    output_dict = dict()
+    output_dict['Date']=date.today()
+    output_dict['Timestamp']=datetime.now(tz=None)    
+    output_dict['Ticker']=ticker
+    output_dict['Price']=price
+    
+    new_data_df = pd.DataFrame(output_dict, index=[0])
+    
+    return new_data_df
+
+def createSlackMessage(new_data_df):
+    timestamp = new_data_df['Timestamp'][0]
+    ticker = new_data_df['Ticker'][0]
+    price = new_data_df['Price'][0]
+    message = f":rocket: ${price} :gorilla: "
+    return message   
+
+def googleSheetAppendDataframe(new_data_df):
+    '''
+    Returns Google Sheets `worksheet` object
+    '''
+
+    KEYFILE = 'service_account_creds.json'
+    worksheet_key = os.getenv("MOONWATCH_WORKSHEET_KEY")
+    sheet_index = 0
+    
+    # Authenticate
+    try:
+        #with open(KEYFILE, "w") as secret_file:
+        #    secret_file.write(service_account_creds)
+        gc = gspread.service_account(filename=KEYFILE)
+        print("Authentication succeeded, yaaay")
+        #os.remove(KEYFILE)
+
+    except:
+        print("Google Sheets authentication failed :( :( :(")
+        
+    # Open the worksheet
+    sh = gc.open_by_key(worksheet_key)
+    worksheet = sh.get_worksheet(sheet_index)
+    
+    # Append new data to the Google Sheet
+    existing = gd.get_as_dataframe(worksheet)
+    updated = existing.append(new_data_df)
+    gd.set_with_dataframe(worksheet, updated)
+    
+    print("Google Sheet updated! In theory, anyway")
+
+
 def post_message_to_slack(text, blocks = None):
     
     slack_token = os.getenv('SLACK_TOKEN')
@@ -29,27 +91,29 @@ def post_message_to_slack(text, blocks = None):
         'icon_emoji': slack_icon_emoji,
         'username': slack_user_name,
         'blocks': json.dumps(blocks) if blocks else None
-    }).json()    
+    }).json() 
 
-def printStomnkPrice(ticker):
+def getStomnkUpdate(ticker):
     
-    # Get current price of stomnk
-    url = f'https://finance.yahoo.com/quote/{ticker}?p={ticker}&.tsrc=fin-srch'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    price = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+    # Get fresh data for the ticker
+    new_data_df = getStomnkPriceDataframe(ticker)
+    
+    # Append data to the Google Sheet
+    googleSheetAppendDataframe(new_data_df)
     
     # Craft a very helpful message
-    message = f''':gorilla: ${price} :rocket:'''        
+    message = createSlackMessage(new_data_df)
     
-    print(message)
+    # Post it to Slack
     post_message_to_slack(message, blocks = None)
+    
+    print("All done")
 
 
 def main():
 
     scheduler = BackgroundScheduler(executors=executors)
-    scheduler.add_job(printStomnkPrice, 'interval', seconds=300, args=["GME"])
+    scheduler.add_job(getStomnkUpdate, 'interval', seconds=600, args=["GME"])
     scheduler.start()
 
     try:
